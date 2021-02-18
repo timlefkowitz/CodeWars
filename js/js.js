c8b14cf180098992a3932e6aecc8b19afa9d158f
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-02-17 23:17:30 -0600

    feat:Codewars Basics

---
+++ js/js.js
@@ -0,0 +1,42 @@
+/*
+        CODE WARS   ////////
+ */
+
+
+
+/*
+ Convert boolean values to strings 'Yes' or 'No'.
+ */
+function boolToWord( bool ){
+    //...
+    if(bool === true){
+        return "Yes";
+    } else {
+        return "No";
+    }
+}
+
+/*
+Convert a Number to a String!
+ */
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString();
+}
+
+/*
+Multiply
+ */
+function multiply(a, b){
+    return a * b;
+}
+
+/*
+
+ */
+
+
+/*
+
+ */
+
